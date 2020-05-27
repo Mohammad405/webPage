42335c58b0401712eb59a59bb9ccd2d6f879c632
Mohammad Azawi <mohammadazawi1@gmail.com>
2020-05-27 01:05:25 -0400

    Add script for exit

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,16 @@
 const first = document.querySelector('#one');
 const projects = document.querySelector('#projects');
-
+const exit = document.querySelector('#close');
 
 
 
 first.addEventListener('click', () => {
     projects.style.display = 'block';
     first.style.display = 'none';
+});
+
+
+exit.addEventListener('click', () => {
+    projects.style.display = 'none';
+    first.style.display = 'block';
 });
